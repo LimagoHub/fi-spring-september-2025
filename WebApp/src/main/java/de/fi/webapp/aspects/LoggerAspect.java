49354cb58d59d5a94d377513bd7d49f1083b24a0
Jo Wagner <jowagner@limago.de>
2025-10-01 12:11:09 +0200

    initial commit

--- WebApp/src/main/java/de/fi/webapp/aspects/LoggerAspect.java
+++ WebApp/src/main/java/de/fi/webapp/aspects/LoggerAspect.java
@@ -4,9 +4,8 @@ package de.fi.webapp.aspects;
 
 import de.fi.webapp.service.PersonenServiceException;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -26,10 +25,22 @@ public class LoggerAspect {
     }
 
     @AfterReturning(value = "execution(public * de.fi.webapp.presentation.v1.PersonenController.*(..))", returning = "result")
-    public void logAdviceAfterReturning(final JoinPoint joinPoint, Object result) throws PersonenServiceException {
+    public void logAdviceAfterReturning(final JoinPoint joinPoint, Object result) {
 
         logger.warning(String.format("############################# Afterreturning: %s ######################", joinPoint.getSignature().getName()));
         logger.warning(String.format("############################# Result: %s ######################", result.toString()));
+    }
+
+    @AfterThrowing(value="execution(public * de.fi.webapp.presentation.v1.PersonenController.*(..))", throwing = "ex")
+    public void afterThrowing(final JoinPoint joinPoint, Throwable ex) {
+
+        logger.warning(String.format("############################# Afterreturning: %s ######################", joinPoint.getSignature().getName()));
+        logger.warning(String.format("############################# Exception: %s ######################", ex.toString()));
+    }
+
+    @Around(value="execution(public * de.fi.webapp.presentation.v1.PersonenController.*(..))")
+    public Object around(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
     }
 
 }
